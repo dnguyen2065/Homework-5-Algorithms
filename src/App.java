c2f51e7390ac6752edc16efdb7ae62408d15f423
dnguyen2065 <dnguyen2065@gmail.com>
2022-12-12 01:33:33 -0500

    some

--- src/App.java
+++ src/App.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Hashtable;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -9,10 +11,12 @@ public class App {
         FileReader file = new FileReader(new File("src\\EdgarAllanPoeBellsB2022groomed.txt"));
         String poem = EAPH.getFileContents(file);
         String[] poemArr = EAPH.stringToCharArray(poem);
-        ArrayList<Character> charArray = EAPH.toCharArr(poemArr);
+        String[] noDupeWords = EAPH.discardDuplicates(poemArr);
+        ArrayList<Character> charArray = EAPH.toCharArr(noDupeWords);
         ArrayList<Integer> asciiArray = EAPH.toAsciiArray(charArray);
         ArrayList<Integer> hashArray = EAPH.toHashArray(asciiArray);
-        System.out.println(poemArr.length);
-
+        Hashtable<Integer, Integer> hashTable = EAPH.toHashTable(hashArray, poemArr);
+        System.out.println(hashArray);
+        // EAPH.printHashTable(hashTable, hashArray);
     }
 }
--- src/EdgarAllanPoeHash.java
+++ src/EdgarAllanPoeHash.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -58,27 +59,76 @@ public class EdgarAllanPoeHash {
     }
 
     public ArrayList<Integer> toHashArray(ArrayList<Integer> asciiArray) {
-        ArrayList<Integer> hashArray = new ArrayList<>();
+        ArrayList<Integer> fullHashArray = new ArrayList<>();
+        ArrayList<Integer> wordHashs = new ArrayList<>();
         int hash = 0;
         int C = 123;
         int m = 293;
         for (int i : asciiArray) {
             if (i == 32) {
                 hash = 0;
+                fullHashArray.add(hash);
             } else {
                 hash = (hash * C + i) % m;
-                hashArray.add(hash);
+                fullHashArray.add(hash);
             }
 
         }
-        return hashArray;
+        for (int i = 0; i < fullHashArray.size(); i++) {
+            if (i == 0) {
+            } else if (fullHashArray.get(i) == 0) {
+                wordHashs.add(fullHashArray.get(i - 1));
+            }
+        }
+
+        return wordHashs;
+    }
+
+    public String[] discardDuplicates(String[] list) {
+        ArrayList<String> newList = new ArrayList<>();
+        ArrayList<String> finList = new ArrayList<>();
+
+        for (String s : list) {
+            newList.add(s);
+        }
+        for (String i : newList) {
+            if (!finList.contains(i)) {
+                finList.add(i);
+            }
+        }
+        String[] wordarr = new String[finList.size()];
+        for (int i = 0; i < finList.size(); i++) {
+            wordarr[i] = finList.get(i);
+        }
+
+        return wordarr;
+    }
+
+    public Hashtable<Integer, Integer> toHashTable(ArrayList<Integer> hashArray, String[] words) {
+        Hashtable<Integer, Integer> hashTable = new Hashtable<>(293);
+        for (int i = 1; i < 293; i++) {
+            for (int j : hashArray)
+                if (i == j) {
+                    hashTable.put(i, j);
+                }
+        }
+        return hashTable;
 
     }
 
-    public HashTable<Integer, String> toHashTable(ArrayList<Integer> hashArray, String[] words) {
-        Hashtable<Integer, String> poemTable = new Hashtable<>(293);
-        for (int i = 0; i < hashArray.size();i++)
-        poemTable.put()
+    public void printHashTable(Hashtable<Integer, Integer> hashTable, ArrayList<Integer> hashArray) {
+        int intvalue = 0;
+        for (int i = 0; i < hashTable.size(); i++) {
+            System.out.println(hashTable.get(i));
+            if (hashTable.get(i) != null) {
+                intvalue = hashTable.get(i);
+            }
+
+            if (intvalue == hashArray.get(i)) {
+                System.out.println("if entered");
+                System.out.println(" " + hashArray.get(i));
+            }
+        }
     }
 
 }
